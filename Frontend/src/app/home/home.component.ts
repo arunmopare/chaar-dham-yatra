470db942335d98a43474f2f3f37275190d875312
Arun Mopare <arunmopare546@gmail.com>
2022-12-01 07:56:46 +0530

    Home componet now shows data

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../interface/place';
+import { AdminService } from '../Services/admin.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  currentPlaces: Place[] = [];
+  search: '';
 
-  ngOnInit() {}
+  constructor(private adminService: AdminService) { }
 
+  ngOnInit() {
+    this.adminService.getSearchedPlaces(undefined).subscribe(
+      res => {
+        this.currentPlaces = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  onSearchModified() {
+    this.adminService.getSearchedPlaces(this.search).subscribe(
+      res => {
+        this.currentPlaces = res;
+        console.log(this.currentPlaces);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
