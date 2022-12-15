1598e40054c877529396bddb7a9a6ae521edf170
Arun Mopare <arunmopare546@gmail.com>
2022-12-15 23:33:06 +0530

    switched to local server.

--- Frontend/src/app/utils/constants/api.constant.ts
+++ Frontend/src/app/utils/constants/api.constant.ts
@@ -1,3 +1,3 @@
-// export const API_ENDPOINT = 'http://localhost:3000/api';
-export const API_ENDPOINT = 'http://192.168.0.101:3000/api';
+export const API_ENDPOINT = 'http://localhost:3000/api';
+// export const API_ENDPOINT = 'http://192.168.0.101:3000/api';
 
